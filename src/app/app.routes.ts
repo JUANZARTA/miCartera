bfefa5f032794ecbac2e9debef0bebbaf3110316
Juan Camilo Zarta Campo <juancamilozartacampo@gmail.com>
2025-02-05 14:06:20 -0500

    Add Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/dashboard/home/home.component';
 import { TransactionsComponent } from './features/dashboard/transactions/transactions.component';
 import { WalletsComponent } from './features/dashboard/wallets/wallets.component';
+import { ExpensesComponent } from './features/dashboard/expenses/expenses.component';
+import { IncomeComponent } from './features/dashboard/income/income.component';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Pantalla de inicio
+  { path: 'home', component: HomeComponent }, // Pantalla de inicio
   { path: 'transactions', component: TransactionsComponent }, // Transacciones
   { path: 'wallets', component: WalletsComponent }, // Billeteras
+  { path: 'expenses', component: ExpensesComponent}, // Gastos
+  { path: 'income', component: IncomeComponent}, // Ingresos
   { path: 'login', component: LoginComponent }, // Login
   { path: 'register', component: RegisterComponent }, // Registro
 ];
