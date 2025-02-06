c0e85e75d3caed00d9be87b98de8ee01a2144b28
Juan Camilo Zarta Campo <108961909+JUANZARTA@users.noreply.github.com>
2025-02-05 21:41:37 -0500

    Add Modules - Loans, Savings, Debts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,27 @@
 import { Routes } from '@angular/router';
 
 // Importamos los componentes Standalone generados
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 import { HomeComponent } from './features/dashboard/home/home.component';
 import { TransactionsComponent } from './features/dashboard/transactions/transactions.component';
 import { WalletsComponent } from './features/dashboard/wallets/wallets.component';
 import { ExpensesComponent } from './features/dashboard/expenses/expenses.component';
 import { IncomeComponent } from './features/dashboard/income/income.component';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
+import { SavingsComponent } from './features/dashboard/savings/savings.component';
+import { LoansComponent } from './features/dashboard/loans/loans.component';
+import { DebtsComponent } from './features/dashboard/debts/debts.component';
 
 export const routes: Routes = [
+  { path: 'login', component: LoginComponent }, // Login
+  { path: 'register', component: RegisterComponent }, // Registro
   { path: 'home', component: HomeComponent }, // Pantalla de inicio
   { path: 'transactions', component: TransactionsComponent }, // Transacciones
   { path: 'wallets', component: WalletsComponent }, // Billeteras
   { path: 'expenses', component: ExpensesComponent}, // Gastos
   { path: 'income', component: IncomeComponent}, // Ingresos
-  { path: 'login', component: LoginComponent }, // Login
-  { path: 'register', component: RegisterComponent }, // Registro
+  { path: 'savings', component: SavingsComponent}, // Ahorros
+  { path: 'loans', component: LoansComponent}, // Prestamos
+  { path: 'debts', component: DebtsComponent}, // Deudas
+  
 ];
