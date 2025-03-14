b324584d54872ef5af9ece565663fbb0f620f885
Juan Camilo Zarta Campo <108961909+JUANZARTA@users.noreply.github.com>
2025-03-14 14:22:56 -0500

    UpDate Responsive Moviles

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  isSidebarOpen = false;
+  isMobileScreen = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkScreenSize(); // Solo ejecuta esto en el navegador
+    }
+  }
+
+  toggleSidebar() {
+    if (this.isMobile()) {
+      this.isSidebarOpen = !this.isSidebarOpen;
+      console.log('Sidebar status:', this.isSidebarOpen);
+    }
+  }
+
+  isMobile(): boolean {
+    return this.isMobileScreen;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkScreenSize();
+    }
+  }
+
+  checkScreenSize() {
+    this.isMobileScreen = window.innerWidth < 1024;
+  }
+  
+  closeSidebar() {
+    if (this.isMobile()) {
+      this.isSidebarOpen = false;
+    }
+  }
 }
